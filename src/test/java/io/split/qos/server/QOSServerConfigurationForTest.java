0d367c13aa2253a7cad9823385587f2dc423631f
Fernando Martin <fernando@split.io>
2016-06-23 10:09:15 -0700

    remove dup code

--- src/test/java/io/split/qos/server/QOSServerConfigurationForTest.java
+++ src/test/java/io/split/qos/server/QOSServerConfigurationForTest.java
@@ -1,26 +1,8 @@
 package io.split.qos.server;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.inject.Singleton;
-import io.dropwizard.Configuration;
-import org.hibernate.validator.constraints.NotEmpty;
 
 @Singleton
-public class QOSServerConfigurationForTest extends Configuration {
+public class QOSServerConfigurationForTest extends QOSServerConfiguration {
 
-    @NotEmpty
-    private String serverName;
-
-    @NotEmpty
-    private String config;
-
-    @JsonProperty
-    public String getServerName() {
-        return serverName;
-    }
-
-    @JsonProperty
-    public String getConfig() {
-        return config;
-    }
 }
