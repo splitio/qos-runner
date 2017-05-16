827e6da6419cd94f272b01a1116853f329578a08
Fernando Martin <fernando@split.io>
2017-05-16 10:39:32 -0700

    fix for pagerduty

--- src/main/java/io/split/qos/server/modules/QOSClientsModule.java
+++ src/main/java/io/split/qos/server/modules/QOSClientsModule.java
@@ -1,6 +1,8 @@
 package io.split.qos.server.modules;
 
 import com.google.inject.AbstractModule;
+import io.split.qos.server.integrations.pagerduty.PagerDutyBroadcaster;
+import io.split.qos.server.integrations.pagerduty.PagerDutyBroadcasterImpl;
 import io.split.qos.server.integrations.slack.broadcaster.SlackBroadcastIntegrationImpl;
 import io.split.qos.server.integrations.slack.broadcaster.SlackBroadcaster;
 
@@ -8,5 +10,6 @@ public class QOSClientsModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(SlackBroadcaster.class).to(SlackBroadcastIntegrationImpl.class);
+        bind(PagerDutyBroadcaster.class).to(PagerDutyBroadcasterImpl.class);
     }
 }
